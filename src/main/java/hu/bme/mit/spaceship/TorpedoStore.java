c9a76f84d0f52a034f980c5315e8a198d0e4f4cd
Végh Iván <6618810+veghivan@users.noreply.github.com>
2025-10-15 17:11:17 +0200

    Took out random from the subroutine and added it as a class attribute

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -12,6 +12,7 @@ public class TorpedoStore {
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
+  private Random generator = new Random();
   private int torpedoCount = 0;
 
   public TorpedoStore(int numberOfTorpedos){
@@ -36,8 +37,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
-    double r = generator.nextDouble();
+    double r = this.generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
